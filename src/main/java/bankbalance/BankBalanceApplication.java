48e62ec66d2a135e0b4c3d3a03f06e76f4fd5bda
Timas Saltanovicius <timas.saltanovicius.2018@rhul.ac.uk>
2020-06-24 20:56:40 +0300

    Initial commit. Created Spring boot maven project.

---
+++ src/main/java/bankbalance/BankBalanceApplication.java
@@ -0,0 +1,13 @@
+package bankbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bankbalance/BankBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package bankbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
