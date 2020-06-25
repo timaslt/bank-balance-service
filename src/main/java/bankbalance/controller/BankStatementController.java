e4f9b5a2abc8d9f837b9617e5c1cf14711e50c25
Timas Saltanovicius <timas.saltanovicius.2018@rhul.ac.uk>
2020-06-25 10:21:17 +0300

    Created BankStatementController class that handles post request for importing bank statement.

---
+++ src/main/java/bankbalance/controller/BankStatementController.java
@@ -0,0 +1,32 @@
+package bankbalance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/** Handles bank statement imports and exports. */
+@RequestMapping("/api")
+@Controller
+public class BankStatementController {
+
+  /**
+   * Handles bank statement import in csv format.
+   *
+   * @param csvFile multipart file that contains bank statement data
+   * @return string response with request status
+   */
+  @PostMapping(value = "/import")
+  @ResponseBody
+  public String importBankStatement(@RequestParam("file") MultipartFile csvFile) {
+    if (!csvFile.getContentType().equals("text/csv")) {
+      return "Input error: wrong file type.";
+    }
+    if (csvFile.isEmpty()) {
+      return "Wrong input: file is empty.";
+    }
+    return "";
+  }
+}
